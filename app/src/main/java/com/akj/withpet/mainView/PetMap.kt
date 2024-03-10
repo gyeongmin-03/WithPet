b3353c571a8db84e1c56d171b5e90793ea392f4a
gyeongmin-03 <lkm1282@naver.com>
2024-03-10 22:35:17 +0900

    줌아웃 시 마커제거

--- app/src/main/java/com/akj/withpet/mainView/PetMap.kt
+++ app/src/main/java/com/akj/withpet/mainView/PetMap.kt
@@ -1,16 +1,22 @@
 package com.akj.withpet.mainView
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.akj.withpet.apiService.MyViewModel
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.compose.CameraPositionState
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.LocationTrackingMode
 import com.naver.maps.map.compose.MapProperties
@@ -39,28 +45,22 @@ fun PetMap(viewModel: MyViewModel) {
         )
     }
 
-    var markerVisibility by remember { mutableStateOf(true) }
-    var cameraPositionState = rememberCameraPositionState().coveringRegion  //이 지역에서 다시 검색을 사용할 때 유용할듯
-    Log.d("테스트", cameraPositionState.toString())
-
-    val isMoving =  rememberCameraPositionState().isMoving
-
     Box(Modifier.fillMaxSize()) {
         NaverMap(
             properties = mapProperties,
             uiSettings = mapUiSettings,
             locationSource = rememberFusedLocationSource(isCompassEnabled = true)
         ){
-            if(!isMoving){
-                viewModel.getPlaceApiData().value?.forEach {place ->
-                    val positionN = place.coordinates.split(", ")[0].substring(startIndex = 1).toDouble()
-                    val positionE = place.coordinates.split(", ")[1].substring(startIndex = 1).toDouble()
-                    Marker(
-                        state = MarkerState(position = LatLng(positionN, positionE))
-                    )
-                }
+            viewModel.getPlaceApiData().value?.forEach {place ->
+                val positionN = place.coordinates.split(", ")[0].substring(startIndex = 1).toDouble()
+                val positionE = place.coordinates.split(", ")[1].substring(startIndex = 1).toDouble()
+                Marker(
+                    state = MarkerState(position = LatLng(positionN, positionE)),
+                    minZoom = 10.0,
+                    captionText = place.title,
+                    captionMinZoom = 10.0
+                )
             }
-
         }
     }
 }
